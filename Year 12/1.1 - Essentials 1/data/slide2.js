7c9dd8fd313e18af981df66f3a6b344993cf6b2b
chamois <grant.pusey@gmail.com>
2016-01-31 10:30:24 +0800

    sds

---
+++ Year 12/1.1 - Essentials 1/data/slide2.js
@@ -0,0 +1,10 @@
+(function(){var loadHandler=window['sl_{CB31F406-9B14-420C-AE7D-F4CA16B6BAD4}'];loadHandler&&loadHandler(1, '<div id="spr0_9d660e5"><div id="spr1_9d660e5" class="kern"><img id="img5_9d660e5" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_9d660e5" style="left:-0.75px;top:-0.563px;"><img id="img0_9d660e5" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_9d660e5" style="left:345px;top:-0.563px;"><img id="img1_9d660e5" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_9d660e5" style="left:-2.751px;top:-1.714px;"><div id="spr6_9d660e5" style="left:0.001px;top:34.523px;"><img id="img2_9d660e5" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_9d660e5" style="left:0.65px;top:40.368px;"><img id="img3_9d660e5" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_9d660e5" class="kern"><div id="spr8_9d660e5" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_9d660e5" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_9d660e5" style="left:46px;top:159px;"><img id="img4_9d660e5" src="data/img6.png" width="625" height="320" alt="(covered in greater depth in different presentation)\
+\
+Projectile motion can be analysed quantitatively by treating the horizontal and vertical components of the motion independently\
+This includes applying the relationships:\
+		𝑣= 𝑠 𝑡   \
+		𝑎= 𝑣−𝑢 𝑡 \
+		𝑣=𝑢+𝑎𝑡   \
+		𝑠=𝑢𝑡+ 1 2 𝑎 𝑡 2   \
+		 𝑣 2 = 𝑢 2 +2𝑎𝑠 		 𝐸 𝑘 = 1 2 𝑚 𝑣 2 \
+"/></div></div></div>');})();
