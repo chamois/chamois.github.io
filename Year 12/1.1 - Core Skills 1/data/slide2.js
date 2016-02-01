2a10dc472d8c5da30b022c4b21d803cd5912e7ce
chamois <grant.pusey@gmail.com>
2016-02-01 13:39:18 +0800

    ff

---
+++ Year 12/1.1 - Core Skills 1/data/slide2.js
@@ -0,0 +1,10 @@
+(function(){var loadHandler=window['sl_{BA90A7E4-A232-4C28-BFDE-72FCCF89ABD6}'];loadHandler&&loadHandler(1, '<div id="spr0_faa2682"><div id="spr1_faa2682" class="kern"><img id="img5_faa2682" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_faa2682" style="left:-0.75px;top:-0.563px;"><img id="img0_faa2682" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_faa2682" style="left:345px;top:-0.563px;"><img id="img1_faa2682" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_faa2682" style="left:-2.751px;top:-1.714px;"><div id="spr6_faa2682" style="left:0.001px;top:34.523px;"><img id="img2_faa2682" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_faa2682" style="left:0.65px;top:40.368px;"><img id="img3_faa2682" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_faa2682" class="kern"><div id="spr8_faa2682" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_faa2682" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_faa2682" style="left:46px;top:159px;"><img id="img4_faa2682" src="data/img6.png" width="625" height="320" alt="(covered in greater depth in different presentation)\
+\
+Projectile motion can be analysed quantitatively by treating the horizontal and vertical components of the motion independently\
+This includes applying the relationships:\
+		𝑣= 𝑠 𝑡   \
+		𝑎= 𝑣−𝑢 𝑡 \
+		𝑣=𝑢+𝑎𝑡   \
+		𝑠=𝑢𝑡+ 1 2 𝑎 𝑡 2   \
+		 𝑣 2 = 𝑢 2 +2𝑎𝑠 		 𝐸 𝑘 = 1 2 𝑚 𝑣 2 \
+"/></div></div></div>');})();
---
+++ Year 12/1.1 - Core Skills 1/data/slide20.js
@@ -0,0 +1,10 @@
+(function(){var loadHandler=window['sl_{BA90A7E4-A232-4C28-BFDE-72FCCF89ABD6}'];loadHandler&&loadHandler(19, '<div id="spr0_faa375b"><div id="spr1_faa375b" class="kern"><img id="img5_faa375b" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_faa375b" style="left:-0.75px;top:-0.563px;"><img id="img0_faa375b" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_faa375b" style="left:345px;top:-0.563px;"><img id="img1_faa375b" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_faa375b" style="left:-2.751px;top:-1.714px;"><div id="spr6_faa375b" style="left:0.001px;top:34.523px;"><img id="img2_faa375b" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_faa375b" style="left:0.65px;top:40.368px;"><img id="img3_faa375b" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_faa375b" class="kern"><div id="spr8_faa375b" style="left:36px;top:23.375px;"><div style="width:0px;"><span id="txt0_faa375b" data-width="415.527344" style="top:23.675px;">Equations of Motion</span></div></div><div id="spr9_faa375b" style="left:45px;top:162px;"><img id="img4_faa375b" src="data/img11.png" width="629" height="303" alt="In Year 12, the equations of motion are the same, but how you use them becomes much more complex.\
+\
+Equations you should already know back–to–front and inside–out:\
+𝑣= 𝑠 𝑡    &amp;   𝑎= ∆𝑣 𝑡 = 𝑣−𝑢 𝑡 \
+Equations you will identify and use to solve motion problems\
+𝑣=𝑢+𝑎𝑡   &amp;     𝑠=𝑢𝑡+ 1 2 𝑎 𝑡 2     &amp;     𝑣 2 = 𝑢 2 +2𝑎𝑠  \
+\
+\
+\
+"/></div></div></div>');})();
