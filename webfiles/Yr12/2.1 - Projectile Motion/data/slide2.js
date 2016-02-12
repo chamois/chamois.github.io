311566f5175edce3c3f65d20bacf3188d3750f87
chamois <grant.pusey@gmail.com>
2016-02-12 15:32:30 +0800

    dsda

---
+++ webfiles/Yr12/2.1 - Projectile Motion/data/slide2.js
@@ -0,0 +1,9 @@
+(function(){var loadHandler=window['sl_{D9F43FFF-8814-498C-AD9F-262C00CBDB86}'];loadHandler&&loadHandler(1, '<div id="spr0_69e25f7"><div id="spr1_69e25f7" class="kern"><img id="img5_69e25f7" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_69e25f7" style="left:-0.75px;top:-0.563px;"><img id="img0_69e25f7" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_69e25f7" style="left:345px;top:-0.563px;"><img id="img1_69e25f7" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_69e25f7" style="left:-2.751px;top:-1.714px;"><div id="spr6_69e25f7" style="left:0.001px;top:34.523px;"><img id="img2_69e25f7" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_69e25f7" style="left:0.65px;top:40.368px;"><img id="img3_69e25f7" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_69e25f7" class="kern"><div id="spr8_69e25f7" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_69e25f7" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_69e25f7" style="left:46px;top:157px;"><img id="img4_69e25f7" src="data/img6.png" width="627" height="335" alt="The movement of free‐falling bodies in Earth’s gravitational field is predictable\
+Projectile motion can be analysed quantitatively by treating the horizontal and vertical components of the motion independently\
+This includes applying the relationships:\
+		𝑣= 𝑠 𝑡   \
+		𝑎= 𝑣−𝑢 𝑡 \
+		𝑣=𝑢+𝑎𝑡   \
+		𝑠=𝑢𝑡+ 1 2 𝑎 𝑡 2   \
+		 𝑣 2 = 𝑢 2 +2𝑎𝑠 		 𝐸 𝑘 = 1 2 𝑚 𝑣 2 \
+"/></div></div></div>');})();
