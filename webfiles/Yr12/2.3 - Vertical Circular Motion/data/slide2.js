eba1ec4c48525c4a3591af31f5803b15de16bc6f
chamois <grant.pusey@gmail.com>
2016-03-06 23:46:07 +0800

    ssds

---
+++ webfiles/Yr12/2.3 - Vertical Circular Motion/data/slide2.js
@@ -0,0 +1,9 @@
+(function(){var loadHandler=window['sl_{BD0BB85F-977B-4B21-92D5-32BCCE3388F3}'];loadHandler&&loadHandler(1, '<div id="spr0_1c231cc3"><div id="spr1_1c231cc3" class="kern"><img id="img5_1c231cc3" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_1c231cc3" style="left:-0.75px;top:-0.563px;"><img id="img0_1c231cc3" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_1c231cc3" style="left:345px;top:-0.563px;"><img id="img1_1c231cc3" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_1c231cc3" style="left:-2.751px;top:-1.714px;"><div id="spr6_1c231cc3" style="left:0.001px;top:34.523px;"><img id="img2_1c231cc3" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_1c231cc3" style="left:0.65px;top:40.368px;"><img id="img3_1c231cc3" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_1c231cc3" class="kern"><div id="spr8_1c231cc3" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_1c231cc3" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_1c231cc3" style="left:45px;top:156px;"><img id="img4_1c231cc3" src="data/img6.png" width="627" height="314" alt="When an object experiences a net force of constant magnitude perpendicular to its velocity, it will undergo uniform circular motion, including circular motion on a horizontal plane and around a banked track; and vertical circular motion\
+This includes applying the relationships:\
+		𝑣= 2𝜋𝑟 𝑇   \
+		 𝑎 𝑐 =  𝑣 2  𝑟 \
+		𝐹=𝑚 𝑎 𝑐 = 𝑚 𝑣 2  𝑟 \
+When a mass moves or is moved from one point to another in a gravitational field and its potential energy changes, work is done on the mass by the field\
+This includes applying the relationships\
+ 𝐸 𝑝 =𝑚𝑔∆ℎ𝑊=𝐹𝑠𝑊=∆𝐸 𝐸 𝑘 = 1 2 𝑚 𝑣 2 \
+"/></div></div></div>');})();
