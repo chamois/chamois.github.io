59842781ec2570f3836d0e2ea8410579b7768f3e
chamois <grant.pusey@gmail.com>
2016-03-11 15:07:34 +0800

    dfghj

---
+++ webfiles/Yr12/2.4 - Gravity Fields/data/slide10.js
@@ -0,0 +1,13 @@
+(function(){var loadHandler=window['sl_{82B3C6C7-4481-469B-B4F1-E8F746F71FDC}'];loadHandler&&loadHandler(9, '<div id="spr0_158038df"><div id="spr1_158038df" class="kern"><img id="img5_158038df" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_158038df" style="left:-0.75px;top:-0.563px;"><img id="img0_158038df" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_158038df" style="left:345px;top:-0.563px;"><img id="img1_158038df" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_158038df" style="left:-2.751px;top:-1.714px;"><div id="spr6_158038df" style="left:0.001px;top:34.523px;"><img id="img2_158038df" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_158038df" style="left:0.65px;top:40.368px;"><img id="img3_158038df" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_158038df" class="kern"><div id="spr8_158038df" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_158038df" data-width="558.496094" style="top:23.675px;">Gravitational Field Strength</span></div></div><div id="spr9_158038df" style="left:45px;top:157px;"><img id="img4_158038df" src="data/img13.png" width="634" height="350" alt="Gravitational Strength is defined as Force per Unit Mass, which might as well be the same as acceleration due to gravity!\
+Start here:\
+ 𝑭 𝒈 = 𝑮𝑴𝒎  𝒓 𝟐  \
+\
+Recall Newton’s 2nd law:\
+ 𝑭 𝒈 =𝒎𝒂=𝒎𝒈\
+So…\
+m𝒈= 𝑮𝑴𝒎  𝒓 𝟐   \
+\
+This simplifies to:\
+𝒈= 𝑮𝑴  𝒓 𝟐  =  𝑭 𝒈  𝒎 = 𝒂 𝒈 \
+\
+Some textbooks will play with these equations in different ways, but it all comes from Newton’s Universal Law of Gravitation!"/></div></div></div>');})();
