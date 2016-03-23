629e73d7bcc729a0929f2a006e519c72076e0742
chamois <grant.pusey@gmail.com>
2016-03-23 17:29:38 +0800

    kk

---
+++ webfiles/Yr12/2.7 - Equilibrium/data/slide2.js
@@ -0,0 +1,11 @@
+(function(){var loadHandler=window['sl_{E41DD21F-864F-4456-9674-DDCB2DDBBB0B}'];loadHandler&&loadHandler(1, '<div id="spr0_303d245f"><div id="spr1_303d245f" class="kern"><img id="img5_303d245f" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_303d245f" style="left:-0.75px;top:-0.563px;"><img id="img0_303d245f" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_303d245f" style="left:345px;top:-0.563px;"><img id="img1_303d245f" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_303d245f" style="left:-2.751px;top:-1.714px;"><div id="spr6_303d245f" style="left:0.001px;top:34.523px;"><img id="img2_303d245f" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_303d245f" style="left:0.65px;top:40.368px;"><img id="img3_303d245f" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_303d245f" class="kern"><div id="spr8_303d245f" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_303d245f" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_303d245f" style="left:45px;top:161px;"><img id="img4_303d245f" src="data/img6.png" width="622" height="279" alt="For a rigid body to be in equilibrium, the sum of the forces and the sum of the moments must be zero.\
+	This includes applying the relationships\
+∑𝐹=0\
+\
+𝜏=𝑟 𝐹𝑠𝑖𝑛𝜃\
+\
+∑𝜏=0\
+\
+\
+\
+"/></div></div></div>');})();
