f8e3286a1711e7f5fa1a9603056cc4a520d603fd
chamois <grant.pusey@gmail.com>
2016-04-10 07:55:21 +0800

    fgsdf

---
+++ webfiles/Yr12/3.1 - Magnetism Intro/data/slide2.js
@@ -0,0 +1,8 @@
+(function(){var loadHandler=window['sl_{ECA2094B-0EFA-4D5E-ACE4-9F3AD306875C}'];loadHandler&&loadHandler(1, '<div id="spr0_e63ac43"><div id="spr1_e63ac43" class="kern"><img id="img5_e63ac43" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_e63ac43" style="left:-0.75px;top:-0.563px;"><img id="img0_e63ac43" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_e63ac43" style="left:345px;top:-0.563px;"><img id="img1_e63ac43" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_e63ac43" style="left:-2.751px;top:-1.714px;"><div id="spr6_e63ac43" style="left:0.001px;top:34.523px;"><img id="img2_e63ac43" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_e63ac43" style="left:0.65px;top:40.368px;"><img id="img3_e63ac43" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_e63ac43" class="kern"><div id="spr8_e63ac43" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_e63ac43" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_e63ac43" style="left:45px;top:161px;"><img id="img4_e63ac43" src="data/img6.png" width="634" height="263" alt="Current‐carrying wires are surrounded by magnetic fields; these fields are utilised in solenoids and electromagnets\
+The strength of the magnetic field produced by a current is a measure of the magnetic flux density\
+This includes applying the relationship:\
+𝐵=  𝜇 0   𝐼 2𝜋 𝑟 \
+Magnetic flux is defined in terms of magnetic flux density and area\
+This includes applying the relationship\
+𝜙=𝐵𝐴\
+"/></div></div></div>');})();
