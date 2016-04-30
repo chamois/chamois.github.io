3e142174369fd457e2afac533a7cc4da4b90e0a9
chamois <grant.pusey@gmail.com>
2016-04-30 09:24:36 +0800

    gg

---
+++ webfiles/Yr12/3.3 - Electric Motors/data/slide2.js
@@ -0,0 +1,12 @@
+(function(){var loadHandler=window['sl_{1BDC6702-87C3-47A5-9241-73A7F00F3779}'];loadHandler&&loadHandler(1, '<div id="spr0_47bdd16a"><div id="spr1_47bdd16a" class="kern"><img id="img5_47bdd16a" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_47bdd16a" style="left:-0.75px;top:-0.563px;"><img id="img0_47bdd16a" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_47bdd16a" style="left:345px;top:-0.563px;"><img id="img1_47bdd16a" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_47bdd16a" style="left:-2.751px;top:-1.714px;"><div id="spr6_47bdd16a" style="left:0.001px;top:34.523px;"><img id="img2_47bdd16a" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_47bdd16a" style="left:0.65px;top:40.368px;"><img id="img3_47bdd16a" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_47bdd16a" class="kern"><div id="spr8_47bdd16a" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_47bdd16a" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_47bdd16a" style="left:45px;top:157px;"><img id="img4_47bdd16a" src="data/img6.png" width="616" height="352" alt="Magnets, magnetic materials, moving charges and current‐carrying wires experience a force in a magnetic field when they cut flux lines; this force is utilised in DC electric motors and particle accelerators\
+\
+This includes applying the relationships\
+ 𝐹=𝑞𝑣𝐵  where 𝑣⊥𝐵\
+\
+ 𝑭=𝑰𝒍𝑩 where 𝒍⊥𝑩\
+\
+The force due to a current in a magnetic field in a DC electric motor produces a torque on the coil in the motor\
+\
+This includes applying the relationship\
+\
+𝝉= 𝒓 ⊥ 𝑭"/></div></div></div>');})();
