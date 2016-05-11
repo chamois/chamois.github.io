131c8c90ff63295deea4bff7ce90aafa7f6eaad6
chamois <grant.pusey@gmail.com>
2016-05-11 12:20:50 +0800

    dfds

---
+++ webfiles/Yr12/3.4 - Induced Current and EMF/data/slide10.js
@@ -0,0 +1,16 @@
+(function(){var loadHandler=window['sl_{52410235-3D41-4EFB-B6EE-490803CEDB29}'];loadHandler&&loadHandler(9, '<div id="spr0_9286112"><div id="spr1_9286112" class="kern"><img id="img5_9286112" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_9286112" style="left:-0.75px;top:-0.563px;"><img id="img0_9286112" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_9286112" style="left:345px;top:-0.563px;"><img id="img1_9286112" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_9286112" style="left:-2.751px;top:-1.714px;"><div id="spr6_9286112" style="left:0.001px;top:34.523px;"><img id="img2_9286112" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_9286112" style="left:0.65px;top:40.368px;"><img id="img3_9286112" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_9286112" class="kern"><div id="spr8_9286112" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_9286112" data-width="542.846680" style="top:23.675px;">Faraday’s Law of Induction</span></div></div><div id="spr9_9286112" style="left:44px;top:156px;"><img id="img4_9286112" src="data/img13.png" width="596" height="350" alt="When a conductor moves through a field, it sweeps out an area, A.\
+∆𝐴=𝑙𝑒𝑛𝑔𝑡ℎ 𝑜𝑓 𝑤𝑖𝑟𝑒×𝑑𝑖𝑠𝑡𝑎𝑛𝑐𝑒=𝑙×𝑣∆𝑡      So 𝒍𝒗= ∆𝑨 ∆𝒕 \
+\
+Remember from previous slide\
+𝐞𝐦𝐟=𝒍𝒗𝑩\
+\
+So combining these, we derive the same equation to use for a loop:\
+emf= 𝐵∆𝐴 ∆𝑡 \
+Now remember that 𝜙=𝐵 𝐴 ⊥ , and considering N loops/coils: \
+𝒆𝒎𝒇=𝐍 ∆𝝓 ∆𝒕 \
+\
+This can be summarised as “The magnitude of the induced EMF in a coil is directly proportional to the rate of change of magnetic flux associated with that coil”\
+\
+\
+\
+"/></div></div></div>');})();
---
+++ webfiles/Yr12/3.4 - Induced Current and EMF/data/slide2.js
@@ -0,0 +1,15 @@
+(function(){var loadHandler=window['sl_{52410235-3D41-4EFB-B6EE-490803CEDB29}'];loadHandler&&loadHandler(1, '<div id="spr0_92852ba"><div id="spr1_92852ba" class="kern"><img id="img5_92852ba" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_92852ba" style="left:-0.75px;top:-0.563px;"><img id="img0_92852ba" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_92852ba" style="left:345px;top:-0.563px;"><img id="img1_92852ba" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_92852ba" style="left:-2.751px;top:-1.714px;"><div id="spr6_92852ba" style="left:0.001px;top:34.523px;"><img id="img2_92852ba" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_92852ba" style="left:0.65px;top:40.368px;"><img id="img3_92852ba" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_92852ba" class="kern"><div id="spr8_92852ba" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_92852ba" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_92852ba" style="left:45px;top:159px;"><img id="img4_92852ba" src="data/img6.png" width="616" height="356" alt="An induced EMF is produced by the relative motion of a straight conductor in a magnetic field when the conductor cuts flux lines\
+ 	This includes applying the relationship\
+induced emf=𝑙𝑣𝐵 where v ⊥ B\
+\
+Magnetic flux is defined in terms of magnetic flux density and area\
+ 	This includes applying the relationship\
+ \
+𝜙=𝐵 𝐴 ⊥ \
+\
+A changing magnetic flux induces a potential difference; this process of electromagnetic induction is used in step‐up and step‐down transformers, DC and AC generators\
+	This includes applying the relationships\
+\
+induced emf=−𝑁   𝜙 2 − 𝜙 1   𝑡 =−𝑁 ∆𝜙 𝑡 =−𝑁 ∆ 𝐵 𝐴 ⊥   t  \
+Conservation of energy, expressed as Lenz’s Law of electromagnetic induction, is used to determine the direction of induced current\
+"/></div></div></div>');})();
