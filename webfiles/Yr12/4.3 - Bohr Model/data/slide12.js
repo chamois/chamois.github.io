23e63f53d26ee12ceafd04d5f62b984053470f3d
chamois <grant.pusey@gmail.com>
2016-08-04 20:44:27 +0800

    fgh

---
+++ webfiles/Yr12/4.3 - Bohr Model/data/slide12.js
@@ -0,0 +1,15 @@
+(function(){var loadHandler=window['sl_{75026678-A340-4962-9931-03C5A405FAE2}'];loadHandler&&loadHandler(11, '<div id="spr0_218002fc"><div id="spr1_218002fc" class="kern"><img id="img5_218002fc" src="data/img6.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_218002fc" style="left:-0.75px;top:-0.563px;"><img id="img0_218002fc" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_218002fc" style="left:345px;top:-0.563px;"><img id="img1_218002fc" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_218002fc" style="left:-2.751px;top:-1.714px;"><div id="spr6_218002fc" style="left:0.001px;top:34.523px;"><img id="img2_218002fc" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_218002fc" style="left:0.65px;top:40.368px;"><img id="img3_218002fc" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_218002fc" class="kern"><div id="spr8_218002fc" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_218002fc" data-width="324.096680" style="top:23.675px;">Remember this:</span></div></div><div id="spr9_218002fc" style="left:23px;top:159px;"><img id="img4_218002fc" src="data/img18.png" width="614" height="332" alt="𝐸=𝑚 𝑐 2  𝑎𝑛𝑑 𝐸=ℎ𝑓 𝑠𝑜 𝑚 𝑐 2 =ℎ𝑓\
+\
+Since 𝑓= 𝑐 𝜆 \
+\
+𝑚 𝑐 2 = ℎ𝑐 𝜆        or       𝑚𝑐= ℎ 𝜆       where c is the velocity of light\
+\
+Since momentum = mass x velocity:\
+\
+𝒑= 𝒉 𝝀  \
+\
+Does this mean that a particle with momentum has a wavelength?\
+\
+\
+\
+"/></div></div></div>');})();
