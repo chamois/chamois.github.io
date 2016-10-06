e5265b776afc986300ddb3ba64a54bb6f53bc6ee
chamois <grant.pusey@gmail.com>
2016-10-06 11:46:50 +0800

    hgf

---
+++ webfiles/Yr08/Physics/3.1 - Simple Machines/data/slide7.js
@@ -0,0 +1,10 @@
+(function(){var loadHandler=window['sl_{04F61961-DACF-47F4-B8C7-1850CB5092B0}'];loadHandler&&loadHandler(6, '<div id="spr0_6ce22966"><div id="spr1_6ce22966" class="kern"><img id="img5_6ce22966" src="data/img9.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_6ce22966" style="left:-0.75px;top:-0.563px;"><img id="img0_6ce22966" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_6ce22966" style="left:345px;top:-0.563px;"><img id="img1_6ce22966" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_6ce22966" style="left:-2.751px;top:-1.714px;"><div id="spr6_6ce22966" style="left:0.001px;top:34.523px;"><img id="img2_6ce22966" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_6ce22966" style="left:0.65px;top:40.368px;"><img id="img3_6ce22966" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_6ce22966" class="kern"><div id="spr8_6ce22966" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_6ce22966" data-width="575.024414" style="top:23.675px;">Mechanical Advantage (MA)</span></div></div><div id="spr9_6ce22966" style="left:44px;top:157px;"><img id="img4_6ce22966" src="data/img17.png" width="630" height="354" alt="We can calculate the mechanical advantage of levers using the following formula\
+\
+Mechanical Advantage = Load Force ÷ Effort Force\
+𝑴𝑨= 𝑳𝑭 𝑬𝑭 \
+\
+Mechanical Advantage = Effort Arm ÷ Load Arm\
+𝑴𝑨= 𝑳𝑨 𝑬𝑨 \
+\
+If the force is amplified, MA is bigger than 1\
+What if MA is smaller than 1? Why would we want this?"/></div></div></div>');})();
