cc44a7d80faa78488a1df39895b581296d73f5bc
chamois <grant.pusey@gmail.com>
2016-12-26 11:55:04 +0800

    gfh

---
+++ webfiles/Yr11/5.1 - Charge and Current/data/slide3.js
@@ -0,0 +1,21 @@
+(function(){var loadHandler=window['sl_{6B32C249-5EE4-47E4-A956-F5F70AF407EA}'];loadHandler&&loadHandler(2, '<div id="spr0_2961e01a"><div id="spr1_2961e01a" class="kern"><img id="img5_2961e01a" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_2961e01a" style="left:-0.75px;top:-0.563px;"><img id="img0_2961e01a" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_2961e01a" style="left:345px;top:-0.563px;"><img id="img1_2961e01a" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_2961e01a" style="left:-2.751px;top:-1.714px;"><div id="spr6_2961e01a" style="left:0.001px;top:34.523px;"><img id="img2_2961e01a" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_2961e01a" style="left:0.65px;top:40.368px;"><img id="img3_2961e01a" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_2961e01a" class="kern"><div id="spr8_2961e01a" style="left:70.335px;top:43.203px;"><div style="width:0px;"><span id="txt0_2961e01a" data-width="299.023438" style="top:20.992px;">Learning Goals</span></div></div><div id="spr9_2961e01a" style="left:40px;top:134px;"><img id="img4_2961e01a" src="data/img8.png" width="583" height="325" alt="Define:\
+Net charge\
+Electric field\
+Electrostatic induction\
+Conductor\
+Insulator\
+Semiconductor\
+Electric current\
+Drift velocity\
+Conventional current\
+Identify that electrons are mobile\
+Identify that like charges repel and unlike charges attract\
+Identify that a neutral object has the same number of positive and negative charges\
+Recognise that charge cannot be created or destroyed, but it can be transferred\
+Recognise that energy flow is only possible in a circuit if the charges are mobile\
+Identify that current flows due to an electric field established by a potential difference\
+Be able to use and rearrange the equation for electric current    𝐼= 𝑞 𝑡  \
+Recognise the direction of an electric current is the direction of the transfer of positive charges\
+Distinguish between conventional current and electron current\
+Give the SI unit for: electric current, time\
+"/></div></div></div>');})();
---
+++ webfiles/Yr11/5.4 - Series and Parallel Circuits/data/slide2.js
@@ -0,0 +1,13 @@
+(function(){var loadHandler=window['sl_{5D45B277-64E4-4426-A613-C4190279E021}'];loadHandler&&loadHandler(1, '<div id="spr0_296321c2"><div id="spr1_296321c2" class="kern"><img id="img5_296321c2" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_296321c2" style="left:-0.75px;top:-0.563px;"><img id="img0_296321c2" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_296321c2" style="left:345px;top:-0.563px;"><img id="img1_296321c2" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_296321c2" style="left:-2.751px;top:-1.714px;"><div id="spr6_296321c2" style="left:0.001px;top:34.523px;"><img id="img2_296321c2" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_296321c2" style="left:0.65px;top:40.368px;"><img id="img3_296321c2" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_296321c2" class="kern"><div id="spr8_296321c2" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_296321c2" data-width="302.587891" style="top:23.675px;">Syllabus points</span></div></div><div id="spr9_296321c2" style="left:46px;top:157px;"><img id="img4_296321c2" src="data/img6.png" width="632" height="321" alt="Circuit analysis and design involve calculation of the potential difference across, the current in, and the power supplied to, components in series, parallel, and series/parallel circuits\
+\
+	This includes applying the relationships for series components:\
+𝐼=𝑐𝑜𝑛𝑠𝑡𝑎𝑛𝑡\
+ 𝑉 𝑡 = 𝑉 1 + 𝑉 2 +… 𝑉 𝑛 \
+ 𝑅 𝑡 = 𝑅 1 + 𝑅 2 +. . . 𝑅 𝑛 \
+\
+	This includes applying the relationships for parallel components:\
+𝑉=𝑐𝑜𝑛𝑠𝑡𝑎𝑛𝑡\
+ 𝐼 𝑡 = 𝐼 1 + 𝐼 2 +… 𝐼 𝑛 \
+ 1  𝑅 𝑡  = 1  𝑅 1  + 1  𝑅 2  +… 1  𝑅 𝑛  \
+\
+"/></div></div></div>');})();
