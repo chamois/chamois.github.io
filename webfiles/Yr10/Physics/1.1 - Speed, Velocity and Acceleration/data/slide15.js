eb47a9d9338a59ab341df35254928218b3a27343
chamois <grant.pusey@gmail.com>
2018-03-06 10:02:16 +0800

    cfgh

---
+++ webfiles/Yr10/Physics/1.1 - Speed, Velocity and Acceleration/data/slide15.js
@@ -0,0 +1,8 @@
+(function(){var loadHandler=window['sl_{45C96248-28A1-40DB-BE0E-C60F832037BB}'];loadHandler&&loadHandler(14, '<div id="spr0_37798ab"><div id="spr1_37798ab" class="kern"><img id="img5_37798ab" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_37798ab" style="left:-0.75px;top:-0.563px;"><img id="img0_37798ab" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_37798ab" style="left:345px;top:-0.563px;"><img id="img1_37798ab" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_37798ab" style="left:-2.751px;top:-1.714px;"><div id="spr6_37798ab" style="left:0.001px;top:34.523px;"><img id="img2_37798ab" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_37798ab" style="left:0.65px;top:40.368px;"><img id="img3_37798ab" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_37798ab" class="kern"><div id="spr8_37798ab" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_37798ab" data-width="254.687500" style="top:23.675px;">Acceleration</span></div></div><div id="spr9_37798ab" style="left:45px;top:157px;"><img id="img4_37798ab" src="data/img33.png" width="490" height="320" alt="Acceleration is the change of VELOCITY with time\
+\
+𝑎=  ∆𝑣 𝑡 =  (𝑣−𝑢) 𝑡  \
+\
+Here:\
+ v = final velocity (at the end)\
+u= initial velocity (at the start)\
+t = time taken"/></div></div></div>');})();
--- webfiles/Yr10/Physics/4.1 - Speed, Velocity and Acceleration/data/slide15.js
+++
@@ -1,8 +0,0 @@
-(function(){var loadHandler=window['sl_{889A60D4-EFC3-462B-93A8-ACB5F4C198EB}'];loadHandler&&loadHandler(14, '<div id="spr0_40274e07"><div id="spr1_40274e07" class="kern"><img id="img5_40274e07" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_40274e07" style="left:-0.75px;top:-0.563px;"><img id="img0_40274e07" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_40274e07" style="left:345px;top:-0.563px;"><img id="img1_40274e07" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_40274e07" style="left:-2.751px;top:-1.714px;"><div id="spr6_40274e07" style="left:0.001px;top:34.523px;"><img id="img2_40274e07" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_40274e07" style="left:0.65px;top:40.368px;"><img id="img3_40274e07" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_40274e07" class="kern"><div id="spr8_40274e07" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_40274e07" data-width="254.687500" style="top:23.675px;">Acceleration</span></div></div><div id="spr9_40274e07" style="left:45px;top:157px;"><img id="img4_40274e07" src="data/img33.png" width="490" height="320" alt="Acceleration is the change of VELOCITY with time\
-\
-𝑎=  ∆𝑣 𝑡 =  (𝑣−𝑢) 𝑡  \
-\
-Here:\
- v = final velocity (at the end)\
-u= initial velocity (at the start)\
-t = time taken"/></div></div></div>');})();
