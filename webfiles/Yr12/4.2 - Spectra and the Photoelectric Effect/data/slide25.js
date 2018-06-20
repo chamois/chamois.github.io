b3ec992c14f4ee5d556ba9df496f4b6b6de869c0
chamois <grant.pusey@gmail.com>
2018-06-20 16:18:11 +0800

    fg

---
+++ webfiles/Yr12/4.2 - Spectra and the Photoelectric Effect/data/slide25.js
@@ -0,0 +1,15 @@
+(function(){var loadHandler=window['sl_{6C556EE0-DFA2-43FF-939F-6EBABF5E84D9}'];loadHandler&&loadHandler(24, '<div id="spr0_1e10f330"><div id="spr1_1e10f330" class="kern"><img id="img5_1e10f330" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_1e10f330" style="left:-0.75px;top:-0.563px;"><img id="img0_1e10f330" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_1e10f330" style="left:345px;top:-0.563px;"><img id="img1_1e10f330" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_1e10f330" style="left:-2.751px;top:-1.714px;"><div id="spr6_1e10f330" style="left:0.001px;top:34.523px;"><img id="img2_1e10f330" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_1e10f330" style="left:0.65px;top:40.368px;"><img id="img3_1e10f330" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_1e10f330" class="kern"><div id="spr8_1e10f330" style="left:36px;top:55.44px;"><div style="width:0px;"><span id="txt0_1e10f330" data-width="277.636719" style="top:23.675px;">Consider this:</span></div></div><div id="spr9_1e10f330" style="left:45px;top:159px;"><img id="img4_1e10f330" src="data/img32.png" width="577" height="327" alt="𝐸=𝑚 𝑐 2  𝑎𝑛𝑑 𝐸=ℎ𝑓 𝑠𝑜 𝑚 𝑐 2 =ℎ𝑓\
+\
+Since 𝑓= 𝑐 𝜆 \
+\
+𝑚 𝑐 2 = ℎ𝑐 𝜆        or       𝑚𝑐= ℎ 𝜆       where c is the velocity of light\
+\
+Since momentum = mass x velocity:\
+\
+𝒑= 𝒉 𝝀  \
+\
+More on this later…\
+\
+\
+\
+"/></div></div></div>');})();
