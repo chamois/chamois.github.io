6531280f61f1dc8e6fdef2dac68b85d5ef901063
chamois <grant.pusey@gmail.com>
2018-07-24 11:32:54 +0800

    kug

---
+++ webfiles/Yr12/5.1 - Special Relativity Introduction/data/slide20.js
@@ -0,0 +1,15 @@
+(function(){var loadHandler=window['sl_{AB2C7F3B-010A-4292-A86B-FF34A7EC6C34}'];loadHandler&&loadHandler(19, '<div id="spr0_232c0d79"><div id="spr1_232c0d79" class="kern"><img id="img5_232c0d79" src="data/img7.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_232c0d79" style="left:-0.75px;top:-0.563px;"><img id="img0_232c0d79" src="data/img0.png" width="721.5" height="82" alt=""/></div><div id="spr4_232c0d79" style="left:345px;top:-0.563px;"><img id="img1_232c0d79" src="data/img1.png" width="375" height="47.812" alt=""/></div><div id="spr5_232c0d79" style="left:-2.751px;top:-1.714px;"><div id="spr6_232c0d79" style="left:0.001px;top:34.523px;"><img id="img2_232c0d79" src="data/img2.png" width="722.775" height="50.106" alt=""/></div><div id="spr7_232c0d79" style="left:0.65px;top:40.368px;"><img id="img3_232c0d79" src="data/img3.png" width="723.63" height="41.02" alt=""/></div></div></div><div id="spr2_232c0d79" class="kern"><div id="spr8_232c0d79" style="left:35.52px;top:37.533px;"><div style="width:0px;"><span id="txt0_232c0d79" data-width="270.483398" style="top:0.294px;">Time Dilation</span></div></div><div id="spr9_232c0d79" style="left:31px;top:105px;"><img id="img4_232c0d79" src="data/img14.png" width="489" height="421" alt="𝑡= 2 𝑐    𝐿 2 +   𝑣𝑡 2   2      and     𝑡 0 = 𝑠 𝑣 = 2𝐿 𝑐    or 𝐿=  ct 0  2 \
+\
+So that means: \
+𝑡= 2 𝑐     𝑐 2  𝑡 0 2   2 2  +   𝑣 2  𝑡 2   2 2    \
+Square both sides\
+\
+ 𝑡 2 =  2 2   𝑐 2    [     𝑐 2  𝑡 0 2   2 2   +   𝑣 2  𝑡 2   2 2    ]   \
+\
+ 𝑡 2 =   𝑡 0 2 +   𝑣 2  𝑡 2   𝑐 2      \
+\
+Factorise:\
+\
+ 𝑡 2  1−  𝑣 2   𝑐 2   =  𝑡 0 2 \
+\
+So to find dilated time:                        𝑡 2 =   𝑡 0 2   1−  𝑣 2   𝑐 2       or        𝐭=   𝒕 𝟎    𝟏−  𝒗 𝟐   𝒄 𝟐      "/></div></div></div>');})();
