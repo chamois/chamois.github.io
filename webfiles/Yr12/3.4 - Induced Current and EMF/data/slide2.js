3c9a62309c24dff88d167d987a66bb1cecf11c94
chamois <grant.pusey@gmail.com>
2022-06-07 19:42:40 +0800

    efsd

--- webfiles/Yr12/3.4 - Induced Current and EMF/data/slide2.js
+++
@@ -1,15 +0,0 @@
-(function(){var loadHandler=window['sl_{B3CD252C-5294-46EF-804D-A6F19D505562}'];loadHandler&&loadHandler(1, '<div id="spr0_9b6db9e"><div id="spr1_9b6db9e" class="kern"><img id="img5_9b6db9e" src="data/img7.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_9b6db9e" style="left:-1px;top:-0.563px;"><img id="img0_9b6db9e" src="data/img0.png" width="962" height="82" alt=""/></div><div id="spr4_9b6db9e" style="left:460px;top:-0.563px;"><img id="img1_9b6db9e" src="data/img1.png" width="500" height="47.812" alt=""/></div><div id="spr5_9b6db9e" style="left:-3.113px;top:-7.459px;"><div id="spr6_9b6db9e" style="left:0px;top:46.009px;"><img id="img2_9b6db9e" src="data/img2.png" width="963.275" height="50.111" alt=""/></div><div id="spr7_9b6db9e" style="left:0.774px;top:51.864px;"><img id="img3_9b6db9e" src="data/img3.png" width="964.465" height="41.023" alt=""/></div></div></div><div id="spr2_9b6db9e" class="kern"><div id="spr8_9b6db9e" style="left:48px;top:55.44px;"><div style="width:0px;"><span id="txt0_9b6db9e" data-width="607.885742" style="top:23.675px;">Syllabus (ATAR Physics Unit 3)</span></div></div><div id="spr9_9b6db9e" style="left:57px;top:157px;"><img id="img4_9b6db9e" src="data/img6.png" width="838" height="310" alt="An induced EMF is produced by the relative motion of a straight conductor in a magnetic field when the conductor cuts flux lines\
- 	This includes applying the relationship\
-induced emf=𝑙𝑣𝐵 where v ⊥ B\
-\
-Magnetic flux is defined in terms of magnetic flux density and area\
- 	This includes applying the relationship\
- \
-𝜙=𝐵 𝐴 ⊥ \
-\
-A changing magnetic flux induces a potential difference; this process of electromagnetic induction is used in step‐up and step‐down transformers, DC and AC generators\
-	This includes applying the relationships\
-\
-induced emf=−𝑁   𝜙 2 − 𝜙 1   𝑡 =−𝑁 ∆𝜙 𝑡 =−𝑁 ∆ 𝐵 𝐴 ⊥   t  \
-Conservation of energy, expressed as Lenz’s Law of electromagnetic induction, is used to determine the direction of induced current\
-"/></div></div></div>');})();
